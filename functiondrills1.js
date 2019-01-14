4ccb81e07fff8de903d3608e65e589b32e365876
Daniel Ors <do7811@gmail.com>
2019-01-14 16:11:13 -0500

    added parameters for createGreeting

--- functiondrills1.js
+++ functiondrills1.js
@@ -1,6 +1,7 @@
 'use strict';
-function createGreeting() {
-  return 'Hi my name is Daniel and I am 29 years old';
+
+function createGreeting(name, age) {
+  return 'Hi my name is ${name} and I am ${age} years old';
 }
 
 const greeting1 = createGreeting();
