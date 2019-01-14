1963c88216cb41e5e06016b2eebb0a2983c6ecf4
Daniel Ors <do7811@gmail.com>
2019-01-14 15:40:14 -0500

    updated greeting with wrapped function

--- functiondrills1.js
+++ functiondrills1.js
@@ -1,1 +1,6 @@
-console.log('Hi my name is Daniel and I am 29 years old');
+'use strict';
+function createGreeting() {
+  console.log('Hi my name is Daniel and I am 29 years old');
+}
+
+createGreeting();
