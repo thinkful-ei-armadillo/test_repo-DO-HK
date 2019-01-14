45a8c1e1839c050d2cfe742b2c03bdade92c78e5
Daniel Ors <do7811@gmail.com>
2019-01-14 16:16:58 -0500

    moved yearOfBirth into its own function

--- functiondrills1.js
+++ functiondrills1.js
@@ -1,10 +1,14 @@
 'use strict';
 
-function createGreeting(name, age) {
-  const yearOfBirth = 2019 - age;
-  return 'Hi my name is ${name} and I am ${age} years old. I was born in ${yearOfBirth}.';
+function getYearOfBirth(age) {
+  return 2019 - age;
 }
 
-createGreeting('Daniel', 29)
+function createGreeting(name, age) {
+  const yob = getYearOfBirth(age);
+  return 'Hi my name is ${name} and I am ${age} years old. I was born in ${yob}.';
+}
+
+createGreeting('Daniel', 29);
 const greeting1 = createGreeting();
-console.log(greeting1)
+console.log(greeting1);
