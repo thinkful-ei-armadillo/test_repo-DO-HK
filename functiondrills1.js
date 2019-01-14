0032538ede3f79a239f6074bf4fc7b72aab7fe46
Daniel Ors <do7811@gmail.com>
2019-01-14 16:14:13 -0500

    added yearOfBirth variable and adjusted the rest of the code accordingly

--- functiondrills1.js
+++ functiondrills1.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function createGreeting(name, age) {
-  return 'Hi my name is ${name} and I am ${age} years old';
+  const yearOfBirth = 2019 - age;
+  return 'Hi my name is ${name} and I am ${age} years old. I was born in ${yearOfBirth}.';
 }
 
+createGreeting('Daniel', 29)
 const greeting1 = createGreeting();
 console.log(greeting1)
