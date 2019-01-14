9f5de936c1596b269e7f3d56fc0568f844488b50
Daniel Ors <do7811@gmail.com>
2019-01-14 17:13:58 -0500

    completed decode function

--- functiondrills2.js
+++ functiondrills2.js
@@ -5,3 +5,46 @@ function jediName(firstName, lastName) {
 }
 
 jediName('Daniel', 'Ors');
+
+function beyond(num) {
+  if ( num > 0 || num < 0 ) {
+    return 'And beyond';
+  }
+  else if ( isFinite(num) === true && num > 0 ) {
+    return 'To infinity';
+  }
+  else if ( isFinite(num) === true && num < 0 ) {
+    return 'To negative infinity';
+  }
+  else if ( num === 0 ) {
+    return 'Staying home';
+  }
+}
+
+beyond(0);
+
+function decode(code) {
+  let decodedLetters = [];
+  let codeArray = code.split(' ');
+  for ( let i = 0; i < codeArray.length - 1; i++ ) {
+    if ( codeArray[i][0] === 'a' ) {
+      decodedLetters.push(codeArray[i][1]);
+    }
+    else if ( codeArray[i][0] === 'b' ) {
+      decodedLetters.push(codeArray[i][2]);
+    }
+    else if ( codeArray[i][0] === 'c' ) {
+      decodedLetters.push(codeArray[i][3]);
+    }
+    else if ( codeArray[i][0] === 'd' ) {
+      decodedLetters.push(codeArray[i][4]);
+    }
+    else {
+      decodedLetters.push(' ');
+    }
+  }
+  return decodedLetters.join('');
+}
+
+decode('craft block argon meter bells brown croon droop');
+
